752e57a5e3050f77b116859e5a28f629a5730ed4
jchapman186 <30930965+jchapman186@users.noreply.github.com>
2017-12-11 08:07:12 -0500

    Working EmptyMap
    This is an EmptyMap that works.

--- EmptyMap.java
+++ EmptyMap.java
@@ -7,18 +7,18 @@ public class EmptyMap
     {
     }
 
-    public void visualize(String region) throws Exception
+    public static void visualize(String region) throws Exception
     {
-
         File f = new File("./input/"+region+".txt");
-        int pointCount = 0;
+
+        double[] min = new double[2];
+        double[] max = new double[2];
         int subCount = 0;
 
         StdDraw.enableDoubleBuffering();  
 
         Scanner inputObject = new Scanner(f);
-        double[] min = new double[2];
-        double[] max = new double[2];
+
         String tempMin = inputObject.nextLine();
         String tempMax = inputObject.nextLine();
         String[] minArr = tempMin.split("   ");
@@ -27,43 +27,38 @@ public class EmptyMap
             min[i] = Double.parseDouble(minArr[i]);
             max[i] = Double.parseDouble(maxArr[i]);
         }
-        
-        
         //0=long 1=lat
-        System.out.println(max[1]);
-        System.out.println(min[1]);
-        System.out.println(max[0]);
-        System.out.println(min[0]);
-        System.out.println(max[1]-min[1]);
- 
-        //end testing
-        StdDraw.setCanvasSize((int) (512*(max[1]-min[1])/(max[0]-min[0])) , 512);
-        
-        StdDraw.setPenRadius(0.05);
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.setXscale(min[1],max[1]);
-        StdDraw.setYscale(min[0],max[0]);
+        System.out.println("Long: " + min[0] + " " + max[0]);
+        System.out.println("Lat: " + min[1] + " " + max[1]);
+        System.out.println("Scale: " + (int) (512*(max[0]-min[0])/(max[1]-min[1])));
+        StdDraw.setCanvasSize((int) (512*(max[0]-min[0])/(max[1]-min[1])), 512);
 
-        int sub = Integer.parseInt(inputObject.nextLine());
-        while(subCount < sub)
+        //StdDraw.setPenRadius(0.05);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setXscale(min[0],max[0]);
+        StdDraw.setYscale(min[1],max[1]);
+        int subs = Integer.parseInt(inputObject.nextLine());
+        System.out.println("#subs: " + subs);
+        while(subCount < subs)
         {
-            subCount++;
-            inputObject.nextLine();
-            inputObject.nextLine();
-            inputObject.nextLine();
-            int pointNo = Integer.parseInt(inputObject.nextLine());
-            pointCount = 0;
-            while(pointCount < pointNo)
+            System.out.println("blank:" + inputObject.nextLine());
+            System.out.println(inputObject.nextLine());
+            System.out.println(inputObject.nextLine());
+            int subLength = Integer.parseInt(inputObject.nextLine());
+            System.out.println("#points: " + subLength);
+            double[] xCoords = new double[subLength];
+            double[] yCoords = new double[subLength];
+            for(int i = 0; i < subLength; i++)
             {
-                pointCount++;
-                String line = inputObject.nextLine();
-                String[] data = line.split("   ");
-                double x = Double.parseDouble(data[0]);
-                double y = Double.parseDouble(data[1]);
-                StdDraw.point(x,y);
+                String[] data = inputObject.nextLine().split("   ");
+                xCoords[i] = Double.parseDouble(data[0]);
+                yCoords[i] = Double.parseDouble(data[1]);
             }
+            subCount++;
+            System.out.println(subCount);
+            StdDraw.polygon(xCoords, yCoords);
         }
         StdDraw.show();
+        inputObject.close();
     }
 }
-
