fcd108c51912f94adbab0c4a261a5b124c7781b2
jchapman186 <30930965+jchapman186@users.noreply.github.com>
2017-12-14 13:41:44 -0500

    Finished ElectoralMap

--- ElectoralMap.java
+++ ElectoralMap.java
@@ -4,42 +4,55 @@ import java.lang.Exception;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
-public class ElectoralMap
-{
-    int[] votes;
-    double[][] coords;
-    private class SubRegion
-    {
+import java.awt.Color;
+
+public class ElectoralMap {
+    public static void main(String[] args) throws Exception {
+        visualize("NV", 2012);
+        System.out.println("Donezo!");
+    }
+
+    static class SubRegion {
         private String name;
-        private int dVotes;
-        private int rVotes;
-        private int iVotes;
-        private double[] xCoords;
-        private double[] yCoords;
-        private String color;
-        
-        private SubRegion(String userName, int[] userVotes, double[][] userCoords, String userColor)
-        {
+        private int[] votes = new int[3]; //0 = rep, 1 = dem, 2 = other
+        private double[] x;
+        private double[] y;
+        private Color color;
+
+        private SubRegion(String userName, double[] userX, double[] userY) {
             name = userName;
-            dVotes = userVotes[0];
-            rVotes = userVotes[1];
-            iVotes = userVotes[2];
-            xCoords = userCoords[0];
-            yCoords = userCoords[1];
-            color = userColor;
+            x = userX;
+            y = userY;
+        }
+        private void addVotes(int[] dataVotes)
+        {
+            votes[0] = dataVotes[0];
+            votes[1] = dataVotes[1];
+            votes[2] = dataVotes[2];
+
+            if(votes[0] > votes[2] && votes[1] > votes[2])
+            {
+             if(votes[0] > votes[1])
+             {
+                 color = Color.RED;
+             }
+             else
+             {
+                 color = Color.BLUE;
+             }
+            }
         }
     }
-    HashMap<String, ArrayList<String>> regions = new HashMap<>();
-    
-    public static void visualize(String region, int year) throws Exception
-    {
-        File f = new File("./input/"+region+year+".txt");
+
+    public static void visualize(String region, int year) throws Exception {
+        File f = new File("./input/" + region + ".txt");
+
+        HashMap<String, ArrayList<SubRegion>> regions = new HashMap<>();
 
         double[] min = new double[2];
         double[] max = new double[2];
-        int subCount = 0;
 
-        StdDraw.enableDoubleBuffering();  
+        StdDraw.enableDoubleBuffering();
 
         Scanner inputObject = new Scanner(f);
 
@@ -47,40 +60,63 @@ public class ElectoralMap
         String tempMax = inputObject.nextLine();
         String[] minArr = tempMin.split("   ");
         String[] maxArr = tempMax.split("   ");
-        for(int i = 0;i<min.length;i++){
+        for (int i = 0; i < min.length; i++) {
             min[i] = Double.parseDouble(minArr[i]);
             max[i] = Double.parseDouble(maxArr[i]);
         }
         //0=long 1=lat
-        StdDraw.setCanvasSize((int) (512*(max[0]-min[0])/(max[1]-min[1])), 512);
+        StdDraw.setCanvasSize((int) (512 * (max[0] - min[0]) / (max[1] - min[1])), 512);
 
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.setXscale(min[0],max[0]);
-        StdDraw.setYscale(min[1],max[1]);
-        int subs = Integer.parseInt(inputObject.nextLine());        
-        while(subCount < subs)
-        {
+        StdDraw.setXscale(min[0], max[0]);
+        StdDraw.setYscale(min[1], max[1]);
+        int subNumber = Integer.parseInt(inputObject.nextLine());
+        for (int s = 0; s < subNumber; s++) {
+
             inputObject.nextLine();
-            inputObject.nextLine();
+            String subName = inputObject.nextLine();
             inputObject.nextLine();
             int subLength = Integer.parseInt(inputObject.nextLine());
             double[] xCoords = new double[subLength];
             double[] yCoords = new double[subLength];
-            for(int i = 0; i < subLength; i++)
-            {
+
+            for (int i = 0; i < subLength; i++) {
                 String[] data = inputObject.nextLine().split("   ");
                 xCoords[i] = Double.parseDouble(data[0]);
                 yCoords[i] = Double.parseDouble(data[1]);
             }
-            subCount++;
+            SubRegion newSub = new SubRegion(subName, xCoords, yCoords);
+            if (regions.containsKey(subName)) {
+                regions.get(subName).add(newSub);
+            }
+            else {
+                ArrayList<SubRegion> subList = new ArrayList<>();
+                subList.add(newSub);
+                regions.put(subName, subList);
+            }
+        }
+
+
+        inputObject.close();
+
+        File newF = new File("./input/" + region + year + ".txt");
+        inputObject = new Scanner(newF);
+        inputObject.nextLine();
+
+        while (inputObject.hasNextLine()) {
+            String[] line = inputObject.nextLine().split(",");
+            String regName = line[0];
+            int[] vote = new int[3];
+            vote[0] = Integer.parseInt(line[1]);
+            vote[1] = Integer.parseInt(line[2]);
+            vote[2] = Integer.parseInt(line[3]);
+            regions.get(regName).get(0).addVotes(vote);
+            for(int i = 0; i < regions.get(regName).size(); i++)
+            {
+                StdDraw.setPenColor(regions.get(regName).get(0).color);
+                StdDraw.filledPolygon(regions.get(regName).get(0).x, regions.get(regName).get(0).y);
+            }
         }
         StdDraw.show();
-        inputObject.close();
-        
-        //reopen file
-        //skip first line
-        //parse thru CSV, assigning the votes to each different SubRegion class
-        //use votes to calculate the color
-        //draw a polygon with changed pencolor
     }
 }
